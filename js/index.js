c8601aba7170d6e41ff4f00ecbbb59f31aa94218
Emine Shaban <emineshaban@icloud.com>
2024-05-22 10:23:06 +0300

    zones

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let acc = document.getElementsByClassName("q");
+var speed = "500";
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.classList.toggle("aa");
+
+            panel.style.display = "none";
+        } else {
+            this.classList.toggle("aa");
+
+            panel.style.display = "block";
+        }
+    });
+}
