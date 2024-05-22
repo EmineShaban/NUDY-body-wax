7d14c8671cd5cec28615e8254484d5df2fe8482b
Emine Shaban <emineshaban@icloud.com>
2024-05-22 11:26:22 +0300

    .

--- js/index.js
+++ js/index.js
@@ -6,10 +6,14 @@ for (i = 0; i < li.length; i++) {
     li[i].addEventListener("click", function (e) {
         for (y = 0; y < text.length; y++) {
             text[y].style.display = "none"; 
-            li[y].style.border = "unset";
+            console.log( li[y].children[0].children[0])
+            li[y].children[0].children[0].style.border = "unset";
+            li[y].children[0].children[1].style.visibility = "hidden";
+
         }
         text[index].style.display = "flex";
-        li[index].style.border = "1px solid #2b2b2b8c";
+        li[index].children[0].children[0].style.border = "1px solid #2b2b2b8c";
+        li[index].children[0].children[1].style.visibility = "visible";
 
 
     });
