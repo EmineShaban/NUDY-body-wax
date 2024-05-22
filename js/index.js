b80b714b9fe5dda4ad12610341cfe40de5e68d8c
Emine Shaban <emineshaban@icloud.com>
2024-05-22 16:12:08 +0300

    comments

--- js/index.js
+++ js/index.js
@@ -18,3 +18,56 @@ for (i = 0; i < li.length; i++) {
 
     });
 }
+
+
+let acc = document.getElementsByClassName("q");
+var speed = "500";
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.classList.toggle("aa");
+
+            panel.style.display = "none";
+        } else {
+            this.classList.toggle("aa");
+
+            panel.style.display = "block";
+        }
+    });
+}
+
+
+
+$(document).ready(function () {
+
+    $(".r-box")
+        .slice(0, 3)
+        .show();
+    $(".r-box:hidden").css("opacity", 0);
+    $("#showmorecomments").on("click", function (e) {
+        $(".r-box:hidden")
+            .slice(0, 3)
+            .slideDown("slow")
+            .animate({
+                opacity: 1
+            }, {
+                queue: false,
+                duration: "slow"
+            });
+        if ($(".r-box:hidden").length == 0) {
+            $("#showmorecomments").hide();
+            $("#showlesscomments").show();
+        }
+        e.preventDefault();
+    });
+
+    $("#showlesscomments").on("click", function (e) {
+        e.preventDefault();
+        $(".r-box").slice(-3).slideUp();
+        $('#showmorecomments').show();
+        $("#showlesscomments").hide();
+    })
+})
