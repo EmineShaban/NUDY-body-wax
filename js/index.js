3ab2a4228d91fc24ab073b330e98e0766ac8a79d
Emine Shaban <emineshaban@icloud.com>
2024-05-22 11:05:25 +0300

    .

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
-let acc = document.getElementsByClassName("q");
-var speed = "500";
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.classList.toggle("aa");
-
-            panel.style.display = "none";
-        } else {
-            this.classList.toggle("aa");
-
-            panel.style.display = "block";
+let li = document.getElementsByClassName("zone-li");
+let text = document.getElementsByClassName("zone-text");
+ 
+for (i = 0; i < li.length; i++) { 
+    let index = i 
+    li[i].addEventListener("click", function (e) {
+        for (y = 0; y < text.length; y++) {
+            text[y].style.display = "none"; 
+            li[y].style.border = "unset";
         }
+        text[index].style.display = "flex";
+        li[index].style.border = "1px solid #2b2b2b8c";
+
+
     });
 }
